e0f4a8ba13ce8ceec6f307485ab050da7b5ec58c
hexuesong <13523012400.h>
2021-04-20 23:05:25 +0800

    UserBean

---
+++ SpringBootTest/src/main/java/com/JavaStudy/dto/User.java
@@ -0,0 +1,23 @@
+package com.JavaStudy.dto;
+
+public class User {
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    private int age;
+}
